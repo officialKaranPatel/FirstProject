b6707cda64fc2524da5d3e61a2c20f2a631e2d4b
Karan patel <pkarand2000@gmail.com>
2022-06-28 12:02:01 -0700

    styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css"; 
 
 const fname = "karan";
 const lname = "patel";
@@ -11,12 +12,14 @@ const links = "https://www.google.com/";
 ReactDOM.render(
     
     <>
-    <h1 contentEditable= "true" > { `first name is ${fname} and last name is ${lname} `} </h1>
+    <h1 className ="heading"> { `first name is ${fname} and last name is ${lname} `} </h1>
+    <div className="img_div">
     <img src={img1} alt='karan'/>
     <img src={img2} alt='karan'/>
     <a href={links}>
     <img src={img3} alt='karan'/>
     </a>
+    </div>
      </>,
     document.getElementById("root")
 
