44a4764ba344f1645c27f8c9e26b1f9e783bd616
Karan patel <pkarand2000@gmail.com>
2022-06-28 09:58:27 -0700

    initalize project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fname = "karan";
+const lname = "patel";
+const currDate = new Date().toLocaleDateString();
+const currTime = new Date().toLocaleTimeString();
+
+
+ReactDOM.render(
+    
+    <>
+    <h1> { `first name is ${fname} and last name is ${lname} `} </h1>
+    <p> current date is = {currDate} </p>
+    <p> current time is = {currTime} </p>
+    
+     </>,
+    document.getElementById("root")
+
+);
