629a07b912231659dc76842524dabad00b500c12
Karan patel <pkarand2000@gmail.com>
2022-07-04 13:11:52 -0700

    challenge 5/7/22

--- src/index.js
+++ src/index.js
@@ -1,37 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css"; 
+import React from 'react';
+import ReactDom from 'react-dom';
+import "./index.css";
+let curDate = new Date(2022,5,5,20);
+curDate = curDate.getHours();
+let greeting = '';
+const cssStyle = {};
 
-const fname = "karan";
-const lname = "patel";
-const img1 =  "https://picsum.photos/200/300";
-const img2 =  "https://picsum.photos/250/300";
-const img3 =  "https://picsum.photos/300/300";
-const links = "https://www.google.com/";
+if(curDate >= 1 && curDate <12){
+    greeting = 'Good Morning';
+    cssStyle.color = 'green';
+}else if (curDate>= 12 && curDate <19){
+    greeting = 'Good Afternoon';
+    cssStyle.color = 'orange';
+}else{
+    greeting = 'Good Night';
+    cssStyle.color = 'blue';
+}
 
-const heading = {
-    color: '#fa9191',
-    textTransform: 'capitalize',
-    textAlign: 'center', 
-    fontWeight: 'bold',
-    textShadow: '0px 2px 4px #ffe9c5',
-    margin: '50px 0px',
-
-};
-
-ReactDOM.render(
-    
+ReactDom.render(
     <>
-    <h1 style= {heading}> { `first name is ${fname} and last name is ${lname} `} </h1>
-    <div className="img_div">
-    <img src={img1} alt='karan'/>
-    <img src={img2} alt='karan'/>
-    <a href={links}>
-    <img src={img3} alt='karan'/>
-    </a>
-    </div>  
-    
-     </>,
-    document.getElementById("root")
+       <div>
+       <h1> Hello sir ,<span style={cssStyle}> {greeting} </span></h1>
+       </div>
+    </>,
+document.getElementById('root'));
 
-);
+
