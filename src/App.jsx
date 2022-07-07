1d5fb02ca63cccbf5bcab6d237508fc2d2d3e629
Karan patel <pkarand2000@gmail.com>
2022-07-07 03:56:21 -0700

    importexport combine

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const cricketer = " virat kohli ";
+
+const favprog = " tmkoc ";
+
+function myName() {
+    let name = "karan";
+   return name;
+}
+
+export default cricketer;
+
+export {favprog , myName};
--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import "./index.css";
-let curDate = new Date(2022,5,5,20);
-curDate = curDate.getHours();
-let greeting = '';
-const cssStyle = {};
+import React from "react";
+import ReactDOM  from "react-dom";
+import myfav,{favprog,myName} from "./App.jsx";
 
-if(curDate >= 1 && curDate <12){
-    greeting = 'Good Morning';
-    cssStyle.color = 'green';
-}else if (curDate>= 12 && curDate <19){
-    greeting = 'Good Afternoon';
-    cssStyle.color = 'orange';
-}else{
-    greeting = 'Good Night';
-    cssStyle.color = 'blue';
-}
-
-ReactDom.render(
+ReactDOM.render(
     <>
-       <div>
-       <h1> Hello sir ,<span style={cssStyle}> {greeting} </span></h1>
-       </div>
+    <ol>
+        <li>kp</li>
+        <li>{myfav}</li>
+        <li>{favprog}</li>
+        <li>{myName()}</li>
+    </ol>
     </>,
-document.getElementById('root'));
-
+    document.getElementById("root")
+);
 
