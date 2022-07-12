dba419826ff450c5fa180b71bd8947ad99c54946
Karan patel <pkarand2000@gmail.com>
2022-07-12 12:55:26 -0700

    events using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-import React from "react";
-import { useState } from "react";
+import {useState} from "react";
+
+
+const App = () => {
+    const purple = '#8e44ad';
+    const [bg, setBg] = useState(purple); 
+    const [name,setName] = useState('click me');
+    const bgchange = () => {
+       // console.log("clicked");
+       let newBg = '#34495e';
+       setBg(newBg);
+       setName('yup');
+
+    };
+    const bgBack = () =>{
+        setBg(purple);
+        setName("ayyo");
+    }
+    return(
+        <> 
+        <div style={{ backgroundColor: bg }}>
+             <button onClick = {bgchange} onDoubleClick ={bgBack}>{name}</button>
+        </div>
+        </>
+    )
+}
+
+export default App;
+/*import { useState } from "react";
 
 
 const App = () =>{
@@ -26,4 +53,4 @@ const App = () =>{
 );
 
     };
-export default App;
+export default App; */
