b5e65c9653ad00dabb1fa81d4221026099c0c518
Karan patel <pkarand2000@gmail.com>
2022-07-14 06:40:03 -0700

    complex form

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,62 @@
 import React from 'react';
 import { useState } from 'react';
 
-const App = () =>{
-   
-    const [name,setName] = useState();
-    const [lastName,setlastName] = useState();
-    const [fullName,setfullName]= useState();
-    const [lastNamenew,setlastNamenew] = useState();
+const App = () => {
 
+    const [fullName, setfullName] = useState({
+        fname : '',
+        lname : '',
 
-    const inputEvent = (Event) =>{  
-        console.log(Event.target.value);
-        setName(Event.target.value);
-        
+    });
+    
+    const inputEvent = (event) => {
+        console.log(event.target.value);
+        console.log(event.target.name);
+
+        const value = event.target.value;
+        const name = event.target.name;
+
+        setfullName ((prevalue) => {
+          //  console.log(prevalue);
+          if (name === "fname") {
+            return {
+                fname : value ,
+                lname : prevalue.lname ,
+            }
+          }else if(name === "lname"){
+            return{
+                lname : value ,
+                fname : prevalue.fname ,
+
+            }
+          }
+
+        });
+
     };
-
-    const onSubmit = (Event) => {
-        Event.preventDefault();
-        setfullName(name);
-        setlastNamenew(lastName);
-    }
     
 
-    const inputEventTwo = (Event) =>{
-        setlastName(Event.target.value); 
-    }
-    return(
-    <>
-      <div className='main_div'>
-        <form onSubmit={onSubmit}>
-            <div>
+    const onSubmit = (event) => {
+        event.preventDefault();
+        alert('form submitted');
+         };
 
-        <h1>hello {fullName} {lastNamenew} </h1>
-        <input type="text"  placeholder="Enter Name" onChange={inputEvent} value={name}/>
-        <br/>
-        <input type="text"  placeholder="Enter Last Name" onChange={inputEventTwo} value={lastName}/>
-        <button type="submit" > click me</button>
-      </div>
-      </form>
-      </div>
-    
-    </>
-    ) 
+    return (
+        <>
+            <div className='main_div'>
+                <form onSubmit={onSubmit}>
+                    <div>
+                        <h1>hello {fullName.fname} {fullName.lname} </h1>
+                        <input type="text" placeholder="Enter Name"  name='fname' onChange={inputEvent} value={fullName.fname}/>
+                        <br />
+                        <input type="text" placeholder="Enter Last Name" name='lname' onChange={inputEvent} value={fullName.lname} />
+                        <br/>
+                       
+                    </div>
+                </form>
+            </div>
+        </>
+    )
 }
 
 export default App;
