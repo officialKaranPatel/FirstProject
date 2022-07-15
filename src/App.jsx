4cf6908b5e94d6a1d67611bed6cd48a576e3c149
Karan patel <pkarand2000@gmail.com>
2022-07-15 08:05:57 -0700

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,53 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import ToDoList from './ToDoList';
+
 
 const App = () => {
 
-    const [fullName, setfullName] = useState({
-        fname : '',
-        lname : '',
-        email : '',
-        phone : '',
-    });
+    const [inputList ,setInputList]= useState("buy apple");
+    const [Items ,setItems]= useState([]);
+
+    const itemEvent = (event) =>{
+     setInputList(event.target.value);
     
-    const inputEvent = (event) => {
-        console.log(event.target.value);
-        console.log(event.target.name);
-
-        const {value,name} = event.target;
-        
-
-        setfullName ((prevalue) => {
-
-            return{
-                ...fullName,
-                [name] : value,
-            }
-          //  console.log(prevalue);
-         /* if (name === "fname") {
-            return {
-                fname : value ,
-                lname : prevalue.lname ,
-                email : prevalue.email ,
-                phone : prevalue.phone ,
-            }
-          }else if(name === "lname"){
-            return{
-                lname : value ,
-                fname : prevalue.fname ,
-                email : prevalue.email ,
-                phone : prevalue.phone ,
-
-            }
-          }else if (name === "email"){
-            return{
-                lname : prevalue.lname ,
-                fname : prevalue.fname ,
-                email : value ,
-                phone : prevalue.phone ,
-
-            }
-          }else if (name === "phone"){
-            return{
-                lname : prevalue.lname ,
-                fname : prevalue.fname ,
-                email : prevalue.email ,
-                phone : value ,
-
-            }
-          }*/
-
-        });
-        
-
+    }
+    const listOfItems = () => {
+        setItems((oldItems) =>{
+            return [...oldItems,inputList];
+        } )
+        setInputList("");
     };
+
+    const deleteItem = (id) =>{
+        setItems((oldItems) =>{
+            return oldItems.filter((arrElem,index)=>{
+                return index !== id;
+            })
+        })
+    };
+    return(
+    <>
+    <div className='main_div'>
+        <div className='center_div'>
+            <br/>
+            <h1>ToDo List</h1>
+            <br/>
+            <input type="text" placeholder="Add a Items" onChange={itemEvent}
+            value={inputList} />
+            <button onClick={listOfItems}> + </button>
+            <ol>
+               { /*<li>{inputList}</li>*/}
+
+               {Items.map((itemval,index)=>{
+                return<ToDoList key={index} id={index} text={itemval} onSelect={deleteItem}/>;
+               })}
+            </ol>
+        </div>
+    </div>
     
-
-    const onSubmit = (event) => {
-        event.preventDefault();
-        alert('form submitted');
-         };
-
-    return (
-        <>
-            <div className='main_div'>
-                <form>
-                    <div>
-                        <h1>hello {fullName.fname} {fullName.lname} </h1>
-                        <p> {fullName.email}  </p>
-                        <p> {fullName.phone} </p>
-                        <input type="text" placeholder="Enter Name"  name='fname' onChange={inputEvent} value={fullName.fname}/>
-                        <br />
-                        <input type="text" placeholder="Enter Last Name" name='lname' onChange={inputEvent} value={fullName.lname} />
-                        <br/>
-                        <input type="text" placeholder="Enter email" name='email' onChange={inputEvent} value={fullName.email} />
-                        <br/>
-                        <input type="text" placeholder="Enter Last Name" name='phone' onChange={inputEvent} value={fullName.phone} />
-                        <br/>
-                        <button onClick={onSubmit}>click me </button>
-                       
-                    </div>
-                </form>
-            </div>
-        </>
+    
+    </>
     )
-}
+};
 
 export default App;
