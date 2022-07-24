c036aab95345831b54c8b39d60fb5f01a252aa14
Karan patel <pkarand2000@gmail.com>
2022-07-24 06:01:44 -0700

    useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { createContext } from "react";
-import ComA from "./ComA"; 
-
-const FirstName = createContext();
-const LastName = createContext();
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const App = ()=> {
+    const[num , setNum]=useState(0);
+    
+    const[nums , setNums]=useState(0);
+
+    useEffect(()=>{
+        alert("i am clicked"); 
+    },[num]);
     return(
     <div>
-        <FirstName.Provider value={"Karan"}>
-            <LastName.Provider value={"patel"}> 
-        <ComA/>
-        </LastName.Provider>
-        </FirstName.Provider>
+        <h1> {num} {nums}</h1>
+        <button onClick={()=>{setNum(num + 1);}}>click me</button>
+        
+        <button onClick={()=>{setNums(nums+ 1);}}>click me</button>
     </div> 
 
-)};
+)}
 
  
 export default App;
-export {FirstName,LastName};
